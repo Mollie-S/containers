5b6e0c704cee640b4469e44c4bf495b055d06750
Olga Samara <olcha.olcha@gmail.com>
2021-10-02 16:38:49 +0200

    some minor updates

---
+++ Vector.hpp
@@ -0,0 +1,22 @@
+#ifdef VECTOR_HPP
+#define VECTOR_HPP
+
+class Vector
+{
+private:
+    /* data */
+public:
+    Vector(/* args */);
+    ~Vector();
+};
+
+Vector::Vector(/* args */)
+{
+}
+
+Vector::~Vector()
+{
+}
+
+
+#endif
---
+++ ft.hpp
@@ -0,0 +1,13 @@
+#ifndef FT_HPP
+#define FT_HPP
+
+#include "Vector.hpp"
+
+namespace ft {
+
+	class Vector;
+	// class Stack;
+	// class Map;
+}
+
+#endif
