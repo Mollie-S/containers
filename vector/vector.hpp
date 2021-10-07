f5cdaa5884a8f43ab772408f811f9a482973e390
Olga Samara <olcha.olcha@gmail.com>
2021-10-07 17:48:50 +0200

    Added constructor and size attributes

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -9,15 +9,27 @@ namespace ft
     template < class T, class Alloc = std::allocator<T> >   // generic template
     class vector
     {
-        typedef T								    value_type;
-        typedef Alloc							    allocator_type;
-        typedef typename allocator_type::reference		reference;
+        typedef T								            value_type;
+        typedef Alloc							            allocator_type;
+        typedef typename allocator_type::reference		    reference;
         typedef typename allocator_type::const_reference	const_reference;
         typedef typename allocator_type::pointer			pointer;
-		typedef typename allocator_type::const_pointer	const_pointer;
-		// add more!!!!!
+		typedef typename allocator_type::const_pointer	    const_pointer;
+
+        // itetators need to be added here:
+		// typedef 	iterator;// convertible to const iterator
+       
+		typedef difference_type iterator_traits<iterator>::difference_type
+		typedef size_type size_t
+
+    // private:
+        size_type _size;
+        size_type _capacity;
+
     public:
-		explicit vector<T, Alloc> (const allocator_type& alloc = allocator_type()) //default
+		explicit vector<T, Alloc> (const allocator_type& alloc = allocator_type())
+        : _size(0)
+        , _capacity(0)//default
         {
             std::cout << "Constructor called\n";
         } 
@@ -32,6 +44,12 @@ namespace ft
 		// vector (const vector& x); // copy (4)
 
         ~vector(){}
+        
+    public:
+        size_type size() const
+        {
+            return _size;
+        }
     };
 };
 
