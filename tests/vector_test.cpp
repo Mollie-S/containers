8210a262c4cf8c4d1e1aa77f002e7a70fc9bac36
Olga Samara <olcha.olcha@gmail.com>
2021-11-19 18:08:41 +0100

    tests for reserve() added, comment for carch2 compilation

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -4,6 +4,8 @@
 #include "../vector/vector.hpp"
 #include <vector>
 
+// add to the command line:
+// -std=c++11
 
 TEST_CASE("Creating an integers container", "[integers]") // [] - is a tag used to select which tests to run
 {
@@ -27,13 +29,19 @@ TEST_CASE("Creating an integers container", "[integers]") // [] - is a tag used 
         REQUIRE(ftVectorInts.size() == 5);
     }
 }
-TEST_CASE("Testing reserve method", "[integers]") // do it for ft_container!!!!
+
+TEST_CASE("Testing reserve method", "[integers]")
 {
-    std::vector<int> stdVectorInts(5, 10);
-    std::vector<int>::iterator it;
-    std::vector<int>::iterator it1;
-    it =  stdVectorInts.begin();
-    stdVectorInts.reserve(100);
-    it1 =  stdVectorInts.begin();
-    REQUIRE(it != it1);
+    ft::vector<int> ftVectorInts(5, 10);
+    size_t capacity_1 = ftVectorInts.capacity();
+    ftVectorInts.reserve(100);
+    size_t capacity_2 = ftVectorInts.capacity();
+    REQUIRE(capacity_1 != capacity_2 );
+    // std::vector<int> stdVectorInts(5, 10);
+    // std::vector<int>::iterator it;
+    // std::vector<int>::iterator it1;
+    // it =  stdVectorInts.begin();
+    // stdVectorInts.reserve(100);
+    // it1 =  stdVectorInts.begin();
+    // REQUIRE(it != it1);
 }
