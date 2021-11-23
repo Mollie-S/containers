2e6eb44d00b5a9a2f66c70ac248e1107beb6dcda
Olga Samara <olcha.olcha@gmail.com>
2021-11-23 17:40:13 +0100

    update

---
+++ vector/iterator_traits.hpp
@@ -0,0 +1,5 @@
+
+// The reason to use != rather than < for testing 
+// whether we have reached the end is partially because
+// that is the more precise statement of what we are testing for 
+// and partially because only randomaccess iterators support <. (Stroupstrup)
