45996cb40cd8dd01981744e02b37fae4465a4d38
Mollie-S <olcha.olcha@gmail.com>
2021-11-30 15:40:00 +0100

    iterator class is moved inside vector

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -10,18 +10,145 @@ namespace ft
     template <class T, class Alloc = ::std::allocator<T> > // generic template
     class vector
     {
-        typedef T                                           value_type; // cannot use "using as it is c++11"
+        //  iterators are an abstraction of pointers
+     	class iterator  // Iterator base class
+        {
+            // This base class only provides some member types, 
+            // which in fact are not required to be present in any iterator type 
+            // (iterator types have no specific member requirements), 
+            // but they might be useful, 
+            //since they define the members needed for the default iterator_traits class template 
+            //to generate the appropriate instantiation automatically 
+            //(and such instantiation is required to be valid for all iterator types).
+
+        public:
+            typedef T 					                    value_type; // cannot use "using as it is c++11"
+            typedef	Alloc			                        allocator_type;
+            typedef size_t 				                    size_type;
+            typedef value_type const&	                    const_reference;
+            typedef value_type& 		                    reference;
+            typedef std::ptrdiff_t		                    difference_type;
+            typedef typename allocator_type::pointer        pointer;
+            typedef typename allocator_type::const_pointer	const_pointer;
+            typedef std::random_access_iterator_tag         iterator_category
+
+        // random_access_iterator -  Empty class to identify the category of an iterator as a random-access iterator:
+
+
+        // DO we define value_type etc fron iterator_traits????::::
+
+        //             value_type, the type denoted by std::iterator_traits<It>::value_type  
+        // difference_type, the type denoted by std::iterator_traits<It>::difference_type
+        // reference, the type denoted by std::iterator_traits<It>::reference
+
+        protected:
+            T*          _ptr;  // T or pointer???
+
+        public:
+            iterator() : _ptr(NULL) {}
+            iterator(T* d): _ptr(d) {}
+            iterator(const iterator& it) : _ptr(it._ptr) {}
+            ~iterator() {};
+            
+            iterator& operator=(const iterator& i)
+            {
+                _ptr = i._ptr;
+                return (*this);
+            }
+
+            reference operator*()
+            {
+                return (*_ptr);
+            }
+
+            reference operator*() const
+            {
+                return (*_ptr);
+            }
+
+            pointer operator->()
+            {
+                return (_ptr);
+            }
+
+            pointer operator->() const
+            {
+                return (_ptr);
+            }
+
+            //  ARITHMETIC OPERATORS
+            iterator& operator++()
+            {
+                _ptr++;
+                return *this;
+            }
+
+            iterator operator++(int) // postfix operator as it accepts an argument
+            {
+                iterator temp = *this;
+                ++(*this);
+                return temp;
+            }
+
+            iterator& operator--()
+            {
+                ptr--;
+                return *this;
+            }
+            
+            iterator operator--(int)
+            {
+                iterator temp = *this;
+                --(*this);
+                return temp;
+            }
+
+
+            iterator& operator+=(const int &val)
+            {
+                ptr += val;
+                return (*this);
+            }
+
+            iterator& operator-=(const int &val)
+            {
+                _ptr -= val;
+                return (*this);
+            }
+
+
+            friend iterator operator+(vector<T, Allocator>::iterator lhs, const int &rhs)
+            {
+                lhs += rhs;
+                return (lhs);
+            }
+
+            friend iterator operator-(vector<T, Allocator>::iterator lhs, const int &rhs)
+            {
+                lhs -= rhs;
+                return (lhs);
+            }
+        
+            friend bool operator==(const iterator& lhs, const iterator& rhs)
+            {
+                return (lhs.ptr == rhs.ptr);
+            }
+            friend bool operator!=(const iterator& lhs, const iterator& rhs) {return !(lhs == rhs);} 
+        };
+
+        // typedef 	iterator;// convertible to const iterator
+
+        typedef typename iterator::value_type value_type;
+
+        typedef T value_type; 
         typedef Alloc                                       allocator_type;
         typedef typename allocator_type::reference          reference;
         typedef typename allocator_type::const_reference    const_reference;
         typedef typename allocator_type::pointer            pointer;
         typedef typename allocator_type::const_pointer      const_pointer;
+        typedef ptrdiff_t                                   difference_type;
+        typedef size_t                                      size_type;
 
-        // itetators need to be added here:
-        // typedef 	iterator;// convertible to const iterator
-
-        typedef ptrdiff_t difference_type;
-        typedef size_t size_type;
 
     private:
         pointer _elements;     // pointer to the first element of the container
@@ -270,6 +397,6 @@ namespace ft
             x._capacity - temp._capacity;
         }
     };
-}
+};
 
 #endif
