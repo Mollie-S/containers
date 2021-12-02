c196a3be57972341f73c802b32caaabf9209ff0c
Olga Samara <olcha.olcha@gmail.com>
2021-12-02 19:09:34 +0100

    main update

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,24 +1,26 @@
 #include "vector.hpp"
 // #include <vector>
 
-// int main(void)
-// {
-//     ft::vector<int> vectorOnStack;
-//     // ft::vector<int> vectorOnStack1(8);
-
-//     // std::vector<int> stdVectorInts(5, 10);
-//     // // std::vector<int>::iterator it;
-//     // // it =  stdVectorInts.begin();
-//     // std::cout << "stdVectorInts capacity: " << stdVectorInts.capacity() << '\n';
-//     // stdVectorInts.reserve(1000000000000);
-//     // std::cout << "stdVectorInts reserved capacity: " << stdVectorInts.capacity() << '\n';
-//     return 0;
-// }
-
 int main(void)
 {
-    ft::vector<int> vectorOnStack;
-    ft::vector<int> vectorOnStack1(8, 6);
-    std::cout << vectorOnStack1.at(10) << std::endl;
+    // ft::vector<int> vectorOnStack1(8);
+
+    // std::vector<int> stdVectorInts(5, 10);
+    // std::vector<int>::iterator it;
+    // it =  stdVectorInts.begin();
+    // std::cout << "stdVectorInts capacity: " << stdVectorInts.capacity() << '\n';
+    // stdVectorInts.reserve(1000000000000);
+    // std::cout << "stdVectorInts reserved capacity: " << stdVectorInts.capacity() << '\n';
+
+    int intArray[] = { 1, 2, 9, 10, 11, 12, 13, 14};
+    size_t arraySize = sizeof(intArray)/ sizeof(intArray[0]);
+    ft::vector<int> myVect(intArray, intArray + arraySize);
+    ft::vector<int>::iterator it;
+    for (it = myVect.begin(); it != myVect.end(); ++it) {
+    // for (it = myVect.begin(); it != it + arraySize; ++it) {
+        std::cout << *it << std::endl;
+    }
+
     return 0;
-}
+} 
+
