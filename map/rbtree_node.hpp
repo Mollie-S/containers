353a95459694a8c1d1b0caac24f7e62614fb605f
Mollie-S <olcha.olcha@gmail.com>
2021-12-26 15:12:22 +0100

    fix - template parameters are removed from the node_base struct

--- map/rbtree_node.hpp
+++ map/rbtree_node.hpp
@@ -22,7 +22,7 @@ namespace ft
 	//Here, we pass the derived class Node<Val> as a template argument to its own base (Node_base).
 	// That allows Node_base to use Node<Val> in its interfaces without even knowing its real name!
 	template <class Key, class T>
-	struct rbtree_node : public rbtree_node_base<rbtree_node<Key, T>
+	struct rbtree_node : public rbtree_node_base
 	{
 		ft::pair<const Key, T> 	_value;
 		explicit rbtree_node(const rbtree_node& value): _value(value) {}
