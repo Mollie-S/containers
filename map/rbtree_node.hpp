baa56528ba90fd9d49dee4a4f6865999a745900f
Mollie-S <olcha.olcha@gmail.com>
2021-12-26 15:04:58 +0100

    rbtree_node struct is split into node_base and node containing its value

--- map/rbtree_node.hpp
+++ map/rbtree_node.hpp
@@ -8,15 +8,24 @@ typedef enum color_t e_color;
 
 namespace ft 
 {
-	template <class Key, class T>
-	struct rbtree_node
+
+
+	struct rbtree_node_base
 	{
-		rbtree_node*				_parent;
-		rbtree_node*				_left;
-		rbtree_node*				_right;
-		e_color 					_color;
-		ft::pair<const Key, T> 		_value;
-		explicit rbtree_node(const rbtree_node& value): _parent(NULL), _left(NULL), _right(NULL), _color(BLACK), _value(value) {}
+		rbtree_node_base*		_parent;
+		rbtree_node_base*		_left;
+		rbtree_node_base*		_right;
+		e_color 				_color;
+
+		rbtree_node_base() : _parent(NULL), _left(NULL), _right(NULL), _color(BLACK) {}
+	};
+	//Here, we pass the derived class Node<Val> as a template argument to its own base (Node_base).
+	// That allows Node_base to use Node<Val> in its interfaces without even knowing its real name!
+	template <class Key, class T>
+	struct rbtree_node : public rbtree_node_base<rbtree_node<Key, T>
+	{
+		ft::pair<const Key, T> 	_value;
+		explicit rbtree_node(const rbtree_node& value): _value(value) {}
 	};
 }
 
