6238dc48bc955736402325c0691469bc37b150a1
Mollie-S <olcha.olcha@gmail.com>
2021-12-29 15:18:07 +0100

    Passing parent and child pointers to the node and node_base constructors

--- map/rbtree_node.hpp
+++ map/rbtree_node.hpp
@@ -16,15 +16,23 @@ namespace ft
 		rbtree_node_base*		_right;
 		e_color 				_color;
 
-		rbtree_node_base() : _parent(NULL), _left(NULL), _right(NULL), _color(BLACK) {}
+		rbtree_node_base(rbtree_node_base* parent_ptr, rbtree_node_base* child_ptr)
+			: _parent(parent_ptr)
+			, _left(child_ptr)
+			, _right(child_ptr)
+			, _color(RED) {}
 	};
+
 	//Here, we pass the derived class Node<Val> as a template argument to its own base (Node_base).
 	// That allows Node_base to use Node<Val> in its interfaces without even knowing its real name!
 	template <typename Value>
 	struct rbtree_node : public rbtree_node_base
 	{
 		Value 	_value;
-		explicit rbtree_node(const Value& value): _value(value) {}
+		explicit rbtree_node(rbtree_node_base* parent_ptr, rbtree_node_base* child_ptr, const Value& value)
+			: rbtree_node_base(parent_ptr, child_ptr)
+			, _value(value)
+			{}
 	};
 }
 
