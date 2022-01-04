0d36cebe366851ddd783c7d451c6bd0db94fb600
Mollie-S <olcha.olcha@gmail.com>
2022-01-04 15:29:04 +0100

    equal is added

--- utility/lexicographical_compare.hpp
+++ utility/lexicographical_compare.hpp
@@ -14,17 +14,18 @@ namespace ft
 	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
                                 InputIterator2 first2, InputIterator2 last2)
 	{
-		while (first1!=last1)
+		while (first1 != last1)
 		{
-			if (first2==last2 || *first2<*first1) 
+			if (first2 == last2 || *first2 < *first1) 
 			{
 				return false;
 			}
-			else if (*first1<*first2) 
+			else if (*first1 < *first2) 
 			{
 				return true;
 			}
-			++first1; ++first2;
+			first1++;
+			first2++;
 		}
 		return (first2!=last2);
 	}
@@ -34,9 +35,9 @@ namespace ft
                                 InputIterator2 first2, InputIterator2 last2,
                                 Compare comp)
 	{
-		while (first1!=last1)
+		while (first1 != last1)
 		{
-			if (first2==last2 || comp(*first2, *first1)) 
+			if (first2 == last2 || comp(*first2, *first1)) 
 			{
 				return false;
 			}
@@ -44,9 +45,10 @@ namespace ft
 			{
 				return true;
 			}
-			++first1; ++first2;
+			first1++; 
+			first2++;
 		}
-		return (first2!=last2);
+		return (first2 != last2);
 	}
 }
 
