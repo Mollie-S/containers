be73c644d060d72e4de74e5f5cd1efa57403494a
Mollie-S <olcha.olcha@gmail.com>
2022-01-06 22:28:46 +0100

    minor changes

--- vector/vector_iterator.hpp
+++ vector/vector_iterator.hpp
@@ -13,9 +13,8 @@ namespace ft
 		//to generate the appropriate instantiation automatically
 		//(and such instantiation is required to be valid for all iterator types).
 	template <class Value>
-	class vector_iter // Iterator base class
+	class vector_iter
 	{
-	
 	public:
 		typedef vector_iter<Value>                                              iterator_type;
 		typedef typename ft::iterator_traits<iterator_type>::iterator_category 	iterator_category;
@@ -24,18 +23,19 @@ namespace ft
     	typedef typename ft::iterator_traits<iterator_type>::pointer           	pointer;
     	typedef typename ft::iterator_traits<iterator_type>::reference         	reference;
 
-			// random_access_iterator -  Empty class to identify the category of an iterator as a random-access iterator:
+		// means that map_iter specialized for any types can get access to the map iter of these types
+		template <typename T>
+		friend class vector_iter;
 
-			//             value_type, the type denoted by std::iterator_traits<It>::value_type
-			// difference_type, the type denoted by std::iterator_traits<It>::difference_type
-			// reference, the type denoted by std::iterator_traits<It>::reference
 	private:
 		pointer _ptr;
-
 	public:
 		vector_iter() : _ptr(NULL) {}
 		vector_iter(pointer ptr) : _ptr(ptr) {}
-		vector_iter(const vector_iter& other) : _ptr(other._ptr) {}
+		// template function inside of a template accepting vector_iter for different value types
+		// this allows to cast from non-const to const
+		template<typename T>
+		vector_iter(const vector_iter<T>& other) : _ptr(other._ptr) {}
 		~vector_iter(){};
 
 		difference_type	distance(vector_iter first, vector_iter last)
@@ -45,6 +45,7 @@ namespace ft
 				count++;
 			return count;
 		}
+
 		vector_iter& operator=(const vector_iter& other)
 		{
 			if (*this != other)
@@ -53,10 +54,12 @@ namespace ft
 			}
 			return (*this);
 		}
+
 		reference operator*() const
 		{
 			return (*_ptr);
 		}
+
 		pointer operator->() const
 		{
 			return (_ptr);
@@ -68,67 +71,99 @@ namespace ft
 			_ptr++;
 			return *this;
 		}
+
 		vector_iter operator++(int) // postfix operator as it accepts an argument
 		{
 			vector_iter temp = *this;
 			++(*this);
 			return temp;
 		}
+
 		vector_iter& operator--()
 		{
 			_ptr--;
 			return *this;
 		}
+
 		vector_iter operator--(int)
 		{
 			vector_iter temp = *this;
 			--(*this);
 			return temp;
 		}
+
 		vector_iter& operator+=(const int &val)
 		{
 			_ptr += val;
 			return (*this);
 		}
+
 		vector_iter& operator-=(const int &val)
 		{
 			_ptr -= val;
 			return (*this);
 		}
+
+
+		// EQUALITY/INEQUALITY OPERATORS:
+		template<typename T>
+		bool operator==(const vector_iter<T>& other) const
+		{
+			return (_ptr == other._ptr);
+		}
+
+		template<typename T>
+		bool operator!=(const vector_iter<T>& other) const
+		{
+			return (_ptr != other._ptr);
+		}
+
+		// TODO: add templated parameters
 		// RELATIONAL OPERATORS
 		bool operator<(const vector_iter& rhs) const
 		{
 			return _ptr < rhs._ptr;
 		}
+
 		bool operator>(const vector_iter& rhs)
 		{
 			return _ptr > rhs._ptr;
 		}
+
 		bool operator<=(const vector_iter& rhs) const
 		{
 			return _ptr <= rhs._ptr;
 		}
+
 		bool operator>=(const vector_iter& rhs)
 		{
 			return _ptr >= rhs._ptr;
 		}
+
 		friend vector_iter operator+(const vector_iter& lhs, const int rhs)
 		{
 			return (lhs._ptr + rhs);
 		}
+
 		friend vector_iter operator-(const vector_iter& lhs, const int rhs)
 		{
 			return (lhs._ptr - rhs);
 		}
+
 		friend difference_type operator-(const vector_iter& lhs, const vector_iter& rhs)
 		{
 			return (lhs._ptr - rhs._ptr);
 		}
+
 		friend bool operator==(const vector_iter& lhs, const vector_iter& rhs)
 		{
 			return (lhs._ptr == rhs._ptr);
 		}
-		friend bool operator!=(const vector_iter& lhs, const vector_iter& rhs) { return !(lhs._ptr == rhs._ptr); }
+
+		friend bool operator!=(const vector_iter& lhs, const vector_iter& rhs) 
+		{
+			return !(lhs._ptr == rhs._ptr);
+		}
 	};
 
 }
