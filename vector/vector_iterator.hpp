b8c6aba79d5fd6c705297700a37feb9835013ed0
Mollie-S <olcha.olcha@gmail.com>
2022-01-10 17:33:57 +0100

    type conversion operator replaced map_iter class friendship

--- vector/vector_iterator.hpp
+++ vector/vector_iterator.hpp
@@ -23,19 +23,14 @@ namespace ft
     	typedef typename ft::iterator_traits<iterator_type>::pointer           	pointer;
     	typedef typename ft::iterator_traits<iterator_type>::reference         	reference;
 
-		// means that map_iter specialized for any types can get access to the map iter of these types
-		template <typename T>
-		friend class vector_iter;
-
 	private:
+		typedef vector_iter<const Value> const_iterator_type;
+
 		pointer _ptr;
 	public:
 		vector_iter() : _ptr(NULL) {}
 		vector_iter(pointer ptr) : _ptr(ptr) {}
-		// template function inside of a template accepting vector_iter for different value types
-		// this allows to cast from non-const to const
-		template<typename T>
-		vector_iter(const vector_iter<T>& other) : _ptr(other._ptr) {}
+		vector_iter(const iterator_type& other) : _ptr(other._ptr) {}
 		~vector_iter(){};
 
 		difference_type	distance(vector_iter first, vector_iter last)
@@ -63,6 +58,12 @@ namespace ft
 		pointer operator->() const
 		{
 			return (_ptr);
+		}
+
+		// type conversion operator
+		operator const_iterator_type() const
+		{
+			return const_iterator_type(_ptr);
 		}
 
 		//  ARITHMETIC OPERATORS
@@ -105,62 +106,49 @@ namespace ft
 		}
 
 
-		// EQUALITY/INEQUALITY OPERATORS:
-		template<typename T>
-		bool operator==(const vector_iter<T>& other) const
-		{
-			return (_ptr == other._ptr);
-		}
-
-		template<typename T>
-		bool operator!=(const vector_iter<T>& other) const
-		{
-			return (_ptr != other._ptr);
-		}
-
 		// TODO: add templated parameters
 		// RELATIONAL OPERATORS
-		bool operator<(const vector_iter& rhs) const
+		bool operator<(const iterator_type& rhs) const
 		{
 			return _ptr < rhs._ptr;
 		}
 
-		bool operator>(const vector_iter& rhs)
+		bool operator>(const iterator_type& rhs)
 		{
 			return _ptr > rhs._ptr;
 		}
 
-		bool operator<=(const vector_iter& rhs) const
+		bool operator<=(const iterator_type& rhs) const
 		{
 			return _ptr <= rhs._ptr;
 		}
 
-		bool operator>=(const vector_iter& rhs)
+		bool operator>=(const iterator_type& rhs)
 		{
 			return _ptr >= rhs._ptr;
 		}
 
-		friend vector_iter operator+(const vector_iter& lhs, const int rhs)
+		friend vector_iter operator+(const iterator_type& lhs, const int rhs)
 		{
 			return (lhs._ptr + rhs);
 		}
 
-		friend vector_iter operator-(const vector_iter& lhs, const int rhs)
+		friend vector_iter operator-(const iterator_type& lhs, const int rhs)
 		{
 			return (lhs._ptr - rhs);
 		}
 
-		friend difference_type operator-(const vector_iter& lhs, const vector_iter& rhs)
+		friend difference_type operator-(const iterator_type& lhs, const iterator_type& rhs)
 		{
 			return (lhs._ptr - rhs._ptr);
 		}
 
-		friend bool operator==(const vector_iter& lhs, const vector_iter& rhs)
+		friend bool operator==(const iterator_type& lhs, const iterator_type& rhs)
 		{
 			return (lhs._ptr == rhs._ptr);
 		}
 
-		friend bool operator!=(const vector_iter& lhs, const vector_iter& rhs) 
+		friend bool operator!=(const iterator_type& lhs, const iterator_type& rhs) 
 		{
 			return !(lhs._ptr == rhs._ptr);
 		}
