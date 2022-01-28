97637054018174944a451cee2b3e529d9082b395
Mollie-S <olcha.olcha@gmail.com>
2022-01-29 00:57:57 +0100

    Updated the path to the executable

--- run_catch_tests.sh
+++ run_catch_tests.sh
@@ -4,5 +4,5 @@ reset=`tput sgr0`
 echo "${green}Compiling Catch2 tests ${reset}"
 make -s catch
 echo "${green}Running Catch2 tests ${reset}"
-cd build && ./catch2_containers_tests
+./build/catch2_containers_tests
 
